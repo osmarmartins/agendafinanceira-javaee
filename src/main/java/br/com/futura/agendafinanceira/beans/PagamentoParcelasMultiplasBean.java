e9508874b885c98e0b32f2b7c2caa21d149f68f8
osmar-X555LF <osmarfilho6@gmail.com>
2019-02-03 19:33:26 -0300

    Ajustes no formulário de parcelamento

--- src/main/java/br/com/futura/agendafinanceira/beans/PagamentoParcelasMultiplasBean.java
+++ src/main/java/br/com/futura/agendafinanceira/beans/PagamentoParcelasMultiplasBean.java
@@ -3,24 +3,25 @@ package br.com.futura.agendafinanceira.beans;
 import java.io.Serializable;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
 
 import br.com.futura.agendafinanceira.dto.ParcelamentoDto;
 import br.com.futura.agendafinanceira.models.Pagamento;
+import br.com.futura.agendafinanceira.models.enums.TipoLancamento;
 import br.com.futura.agendafinanceira.services.PagamentoParcelaService;
 import br.com.futura.agendafinanceira.utils.MessagesHelper;
 
-@Model
+@Named
+@ViewScoped
 public class PagamentoParcelasMultiplasBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	private Pagamento pagamento;
-	
-	private Integer qtdParcelas;
-	
+
 	@Inject
 	private ParcelamentoDto parcelamento;
 
@@ -33,36 +34,17 @@ public class PagamentoParcelasMultiplasBean implements Serializable {
 	@PostConstruct
 	private void init() {
 		this.parcelamento = new ParcelamentoDto();
-
-//		System.out.println("INIT PAGAMENTO " + this.pagamento);
-//
-//		this.parcelamento = new ParcelamentoDto(this.pagamento);
-//		this.parcelamento.setPrimeiroVencimento(new Date(System.currentTimeMillis()));
-//
-//		this.parcelamento.setQuantidadeParcelas(4);
-//		this.parcelamento.setValorParcela(new BigDecimal(150));
-//		this.parcelamento.setIntervaloDias(15);
-//		this.parcelamento.setTipoLancamento(TipoLancamento.MENSAL);
-//
-//		System.out.println(">>>>>>>>>>>. POST_CONSTRUCTOR " + this.parcelamento);
+		this.parcelamento.setTipoLancamento(TipoLancamento.MENSAL);
 	}
 
 	public String gerarParcelas() {
+		this.parcelamento.setPagamento(pagamento);
+
 		// TODO - Gerar parcelas de pagamento de acordo com formulário de parcelamento
 		// parcelaService.salvar(pagamento, parcela);
 
-//		if (this.parcelamento != null) {
-//			this.parcelamento.setPagamento(pagamento);
-//			this.parcelamento.setQuantidadeParcelas(qtdParcelas);
-//			
-//		}else {
-//			System.out.println("NULL <<<<<<<<<<<<<<<<<<<<< ");
-//		}
-		
 		System.out.println(">>>>>>>> GERACAO DAS PARCELAS " + this.parcelamento);
 
-		this.parcelamento.setPagamento(pagamento);
-		
 		messagesHelper.addFlash(new FacesMessage("Parcelas geradas com sucesso!" + this.parcelamento));
 		return "/pagamento/pagamentocadastro?faces-redirect=true" + "&pagamento=" + this.pagamento.getIdPagamento();
 	}
@@ -72,29 +54,18 @@ public class PagamentoParcelasMultiplasBean implements Serializable {
 	}
 
 	public Pagamento getPagamento() {
-		if (pagamento == null) {
-			this.pagamento = new Pagamento();
-		}
 		return pagamento;
 	}
 
 	public ParcelamentoDto getParcelamento() {
-		if (this.parcelamento == null) {
-			this.parcelamento = new ParcelamentoDto(this.pagamento);
-		}
+//		if (this.parcelamento == null) {
+//			this.parcelamento = new ParcelamentoDto(this.pagamento);
+//		}
 		return this.parcelamento;
 	}
 
 	public void setParcelamento(ParcelamentoDto parcelamento) {
 		this.parcelamento = parcelamento;
-	}
-
-	public Integer getQtdParcelas() {
-		return qtdParcelas;
-	}
-
-	public void setQtdParcelas(Integer qtdParcelas) {
-		this.qtdParcelas = qtdParcelas;
 	}
 
 }
