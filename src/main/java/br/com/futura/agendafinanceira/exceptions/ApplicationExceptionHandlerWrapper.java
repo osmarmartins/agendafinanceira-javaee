bd748ef05b38805f25976f7b946264a1f8f41602
osmar-X555LF <osmarfilho6@gmail.com>
2019-02-22 05:48:16 -0300

    Inicio da refatoração das classes de manipulação de exceção

--- src/main/java/br/com/futura/agendafinanceira/exceptions/ApplicationExceptionHandlerWrapper.java
+++ src/main/java/br/com/futura/agendafinanceira/exceptions/ApplicationExceptionHandlerWrapper.java
@@ -5,6 +5,7 @@ import java.util.Iterator;
 import java.util.Map;
 
 import javax.faces.FacesException;
+import javax.faces.application.FacesMessage;
 import javax.faces.application.NavigationHandler;
 import javax.faces.application.ViewExpiredException;
 import javax.faces.context.ExceptionHandler;
@@ -13,112 +14,87 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.ExceptionQueuedEventContext;
-import javax.persistence.PersistenceException;
-import javax.transaction.RollbackException;
 
 public class ApplicationExceptionHandlerWrapper extends ExceptionHandlerWrapper {
 
-    private ExceptionHandler wrapped;
+	private ExceptionHandler wrapped;
 
-    public ApplicationExceptionHandlerWrapper(ExceptionHandler wrapped) {
-        this.wrapped = wrapped;
-    }
-
-    @Override
-    public ExceptionHandler getWrapped() {
-        return this.wrapped;
-    }
-
-    @Override
-    public void handle() throws FacesException {
-        Iterator<ExceptionQueuedEvent> events = getUnhandledExceptionQueuedEvents().iterator();
-
-        while (events.hasNext()) {
-            ExceptionQueuedEvent event = events.next();
-            ExceptionQueuedEventContext context = (ExceptionQueuedEventContext) event.getSource();
-            Map<String, Object> request = FacesContext.getCurrentInstance().getExternalContext().getRequestMap();
-
-            Throwable exception = context.getException();
-
-            boolean handled = false;
-
-            try {
-            	if (exception instanceof ViewExpiredException) {
-                    handled = true;
-                    redirect("/login.xhtml");
-                } else if (exception instanceof RollbackException) {
-                	request.put("error-message", "Não foi possível realizar a operção no banco de dados.");                	
-                    handled = true;
-                    redirect("/erro.xhtml");
-                } else {
-                	request.put("error-message", exception.getMessage());                	
-                	handled = true;
-                    redirect("/erro.xhtml");
-                }
-            } finally {
-                if (handled) {
-                    events.remove();
-                }
-            }
-        }
-
-        getWrapped().handle();
-    }
-
-    private void redirect(String page) {
-        try {
-            FacesContext facesContext = FacesContext.getCurrentInstance();
-            ExternalContext externalContext = facesContext.getExternalContext();
-            String contextPath = externalContext.getRequestContextPath();
-
-            externalContext.redirect(contextPath + page);
-            facesContext.responseComplete();
-        } catch (IOException e) {
-            throw new FacesException(e);
-        }
-    }	
+	private final FacesContext facesContext = FacesContext.getCurrentInstance();
+	private final Map<String, Object> requestMap = facesContext.getExternalContext().getRequestMap();
+	private final NavigationHandler navigationHandler = facesContext.getApplication().getNavigationHandler();
 	
-	
-	
-	
-	
-//    private ExceptionHandler handler;
-//    
-//    public ApplicationExceptionHandlerWrapper(ExceptionHandler handler) {
-//        this.handler = handler;
-//    }
-// 
-//    @Override
-//    public ExceptionHandler getWrapped() {
-//        return handler;
-//    }
-// 
-//    @Override
-//    public void handle() throws FacesException {
-//        final Iterator<ExceptionQueuedEvent> event = getUnhandledExceptionQueuedEvents().iterator();
-// 
-//        while (event.hasNext()) {
-//            Throwable throwable = event.next().getContext().getException();
-// 
-//            final FacesContext context = FacesContext.getCurrentInstance();
-//            final NavigationHandler navigation = context.getApplication().getNavigationHandler();
-//            final Map<String, Object> request = context.getExternalContext().getRequestMap();
-// 
-//            try {
-//            	if (throwable.equals(javax.persistence.PersistenceException.class) ) {
-//            		request.put("error-message", "Não foi possível realizar a operção no banco de dados.");
-//            	}else {
-//            		request.put("error-message", throwable.getMessage());
-//            	}
-//            		
-//                navigation.handleNavigation(context, null, "/erro");
-//                context.renderResponse();
-//            } finally {
-//                event.remove();
-//            }
-//        }
-// 
-//        getWrapped().handle();
-//    }	
+	private String mensagemErro;
+
+	public ApplicationExceptionHandlerWrapper(ExceptionHandler wrapped) {
+		this.wrapped = wrapped;
+	}
+
+	@Override
+	public ExceptionHandler getWrapped() {
+		return this.wrapped;
+	}
+
+	@Override
+	public void handle() throws FacesException {
+		Iterator<ExceptionQueuedEvent> iterator = getUnhandledExceptionQueuedEvents().iterator();
+
+		while (iterator.hasNext()) {
+			ExceptionQueuedEvent event = iterator.next();
+			ExceptionQueuedEventContext context = (ExceptionQueuedEventContext) event.getSource();
+			Throwable exception = context.getException();
+
+			boolean handled = false;
+
+			try {
+
+				requestMap.put("error-message", exception.getMessage());
+
+				if (isException(exception, "ConstraintViolationException")) {
+					handled = true;
+					this.mensagemErro = "Registro não pode ser removido! Verifique suas associações.";
+					redirect("/erro.xhtml");
+				} else if (isException(exception, "org.hibernate.StaleObjectStateExcetion")) {
+					handled = true;
+					this.mensagemErro = "Registro alterado por outro usuário! Atualize os dados e repita a operação.";
+					redirect("/erro.xhtml");
+				}else if (exception instanceof ViewExpiredException) {
+					handled = true;
+					redirect("/login.xhtml");
+				} else {
+					handled = true;
+					redirect("/erro.xhtml");
+				}
+				
+//				navigationHandler.handleNavigation(context, fromAction, outcome, toFlowDocumentId);
+				
+			} finally {
+				if (handled) {
+					iterator.remove();
+				}
+			}
+		}
+
+		getWrapped().handle();
+	}
+
+	private void redirect(String page) {
+		try {
+			
+			
+			FacesContext facesContext = FacesContext.getCurrentInstance();
+			ExternalContext externalContext = facesContext.getExternalContext();
+			String contextPath = externalContext.getRequestContextPath();
+
+			facesContext.addMessage("mensagemErro", new FacesMessage(FacesMessage.SEVERITY_ERROR, mensagemErro, ""));
+			externalContext.redirect(contextPath + page);
+			facesContext.responseComplete();
+		} catch (IOException e) {
+			throw new FacesException(e);
+		}
+	}
+
+	private boolean isException(Throwable exception, String message) {
+		return exception != null && exception.getMessage() != null && exception.getMessage().indexOf(message) != -1;
+	}
 
 }
